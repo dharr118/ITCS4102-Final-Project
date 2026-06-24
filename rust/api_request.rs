542d49d29dab5ca179ab9ee113cb6f3e898618fb
dharr118 <dharr118@charlotte.edu>
2026-06-23 21:58:28 -0400

    Update api_request.rs

--- rust/api_request.rs
+++ rust/api_request.rs
@@ -1,10 +1,11 @@
-// This example is intended to be used inside a Cargo project with reqwest installed.
-// Cargo.toml dependency example:
-// reqwest = { version = "0.12", features = ["blocking"] }
+use reqwest;
+use tokio;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let response = reqwest::blocking::get("https://api.github.com")?;
-    println!("Status code: {}", response.status());
-    println!("Content type: {:?}", response.headers().get("content-type"));
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let response = reqwest::get("https://api.github.com").await?;
+
+    println!("Status Code: {}", response.status());
+
     Ok(())
 }
