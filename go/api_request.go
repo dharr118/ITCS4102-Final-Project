1827672fb612b63b4047583341f8e57ae538623f
dharr118 <dharr118@charlotte.edu>
2026-06-23 21:57:49 -0400

    Update api_request.go

--- go/api_request.go
+++ go/api_request.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    response, err := http.Get("https://api.github.com")
-    if err != nil {
-        fmt.Println("Request error:", err)
-        return
-    }
-    defer response.Body.Close()
+	response, err := http.Get("https://api.github.com")
 
-    fmt.Println("Status code:", response.StatusCode)
-    fmt.Println("Content type:", response.Header.Get("Content-Type"))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println("Status Code:", response.StatusCode)
 }
